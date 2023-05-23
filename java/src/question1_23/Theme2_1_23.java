3cd6fcb1b2e74d7b903b6421e0b11307537560c2
mm9sk <9misa25@gmail.com>
2023-05-23 21:11:03 +0900

    1.23 カプセル化とゲッターとセッター

---
+++ java/src/question1_23/Theme2_1_23.java
@@ -0,0 +1,65 @@
+package question1_23;
+
+/*
+	
+	下記がコンソールに出力されるように作成してください
+	※thisとsetterとgetterとフィールドを使ってください
+	
+	動物名：ライオン
+	体長：2.1m
+	速度：80km/h
+	
+*/
+
+public class Theme2_1_23 {
+	// フィールドに各項目の変数を定義
+	private String name;
+	private double length;
+	private int speed;
+
+	// getterとsetter
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setLenght(double length) {
+		this.length = length;
+	}
+
+	public double getLenght() {
+		return length;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public static void main(String[] args) {
+		Theme2_1_23 gn = new Theme2_1_23();
+
+		// nameに"ライオン"を代入
+		gn.setName("ライオン");
+		// コンソールに"動物名：ライオン"と表示
+		System.out.println("動物名：" + gn.getName());
+
+		// lengthに"2.1"を代入
+		gn.setLenght(2.1);
+		// コンソールに"体長：2.1m"と表示
+		System.out.println("体長：" + gn.getLenght() + "m");
+
+		// speedに"ライオン"を代入
+		gn.setSpeed(80);
+		// コンソールに"速度：80km/h"と表示
+		System.out.println("速度：" + gn.getSpeed() + "km/h");
+
+	}
+
+}
