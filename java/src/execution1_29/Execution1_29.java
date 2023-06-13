9661c18f75308baeff5c73389dbcfa3b46a9f0b7
mm9sk <9misa25@gmail.com>
2023-06-14 00:02:45 +0900

    1.29 オブジェクト指向の基本②

---
+++ java/src/execution1_29/Execution1_29.java
@@ -0,0 +1,12 @@
+package execution1_29;
+
+import process1_29.Process1_29;
+
+public class Execution1_29 {
+
+	public static void main(String[] args) {
+		// Process1_29を呼び出し
+		Process1_29 pro = new Process1_29();
+		pro.display();
+	}
+}
