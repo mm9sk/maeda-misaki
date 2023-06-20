19332517c2349bccdbc0fd0f7e4c65f817350283
mm9sk <9misa25@gmail.com>
2023-06-20 23:09:50 +0900

    課題1_32

---
+++ java/src/question1_32/Main.java
@@ -0,0 +1,26 @@
+package question1_32;
+
+class Main {
+	public static void main(String[] argos) {
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		person1.print();
+
+		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		person2.print();
+
+		// 問題3：Main.javaで System.out.printlnを使い「合計〇〇人です」と出力してください。〇〇はcountの値です。
+		System.out.println("合計" + (person1.count + person2.count) + "人です\n");
+		
+		// 問題6：クラスメソッド「printCount」を呼び出してください
+		Sub.printCount();
+	}
+}
+
+class Sub {
+	// 問題4：クラスメソッド「printCount」を定義してください（データ型：void）
+	public static void printCount() {
+		// 問題5：クラスメソッドの中にクラスフィールド「count」を用いて「合計〇〇人です」と出力してください
+		int count = 2;
+		System.out.println("合計" + count + "人です");
+	}
+}
---
+++ java/src/question1_32/Person.java
@@ -0,0 +1,29 @@
+package question1_32;
+
+class Person {
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	// 問題1：クラスフィールド「count」を定義してください（初期値：0,データ型：int）
+	int count = 0;
+
+	Person(String name, int age, double height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		// 問題2：Personコンストラクタの中でクラスフィールドcountに1を足してください
+		this.count++;
+	}
+
+	public double bmi() {
+		return this.weight / this.height / this.height;
+	}
+
+	public void print() {
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "です");
+		System.out.println("合計" + this.count + "人です\n");
+	}
+}
